2f8a3375a2deb126d8cbd72f5241cddb365a699f
Gurleen Sethi <sarusethi@rocketmail.com>
2020-02-10 13:43:44 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./styles.css";
+
+const App: React.FunctionComponent<{}> = props => {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+};
+
+export default App;
